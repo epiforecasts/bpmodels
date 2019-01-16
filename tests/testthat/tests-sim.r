2308ff3e57081f74075e0d1cdfb7937f825ded80
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2019-01-16 10:42:52 +0000

    fix simulation test

--- tests/testthat/tests-sim.r
+++ tests/testthat/tests-sim.r
@@ -3,7 +3,7 @@ context("Simulating from a branching process model")
 test_that("Chains can be simulated",
 {
     expect_length(chain_sim(n=2, "pois", lambda=0.5), 2)
-    expect_length(chain_sim(n=10, "pois", "length", lambda=0.9), 2)
+    expect_length(chain_sim(n=10, "pois", "length", lambda=0.9), 10)
     expect_false(any(is.finite(chain_sim(n=2, "pois", "length", lambda=0.5, infinite=1))))
 })
 
