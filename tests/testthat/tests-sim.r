931d6802d80b624bdd7e558e2ae0d999405348eb
James Azam <jamesazam@sun.ac.za>
2023-01-10 17:22:40 +0000

    added more unit tests to improve coverage

--- tests/testthat/tests-sim.r
+++ tests/testthat/tests-sim.r
@@ -8,12 +8,33 @@ test_that("Chains can be simulated",
                                         infinite=10)))
     expect_false(any(is.finite(chain_sim(n=2, "pois", "length", lambda=0.5,
                                          infinite=1))))
+    expect_no_error(chain_sim(n = 2, offspring = 'pois', "size", lambda = 0.9, 
+                                               tree = TRUE)
+                    )
 })
 
 test_that("Errors are thrown",
 {
     expect_error(chain_sim(n=2, "dummy"), "does not exist")
     expect_error(chain_sim(n=2, "lnorm", meanlog=log(1.6)), "integer")
+    expect_error(chain_sim(n = 2, offspring = pois, "length", lambda = 0.9), 
+                 "not found"
+                 )
+    expect_error(chain_sim(n = 2, offspring = 'pois', "size", lambda = 0.9, 
+                           serial = c(1:2), "must be a function")
+                 )
+    expect_error(chain_sim(n = 2, offspring = c(1, 2), "length", lambda = 0.9),
+                 "not a character string")
+    expect_error(chain_sim(n = 2, offspring = list(1, 2), "length", lambda = 0.9),
+                 "not a character string")
+    expect_error(chain_sim(n = 2, offspring = 'pois', "size", lambda = 0.9, 
+                           serial = function(x) rpois(x, 0.9), tree = FALSE),
+                 "If `serial` is specified, then `tree` cannot be set to `FALSE`."
+                 )
+    expect_error(chain_sim(n = 2, offspring = 'pois', "size", lambda = 0.9, 
+                           tf = 5, tree = FALSE),
+                 "If `tf` is specified, `serial` must be specified too."
+    )
 })
 
 context("Simulating from a branching process model
@@ -110,4 +131,18 @@ test_that("Errors are thrown",
             ),
         "argument \"disp_offspring\" is missing, with no default")
 
+})
+
+test_that('warnings work as expected', {
+  expect_warning(
+    chain_sim_susc(
+      "pois",
+      mn_offspring = 3,
+      disp_offspring = 1,
+      serial = function(x) 3,
+      pop = 100
+      ),
+    "argument disp_offspring not used for
+                poisson offspring distribution."
+    )
 })
