dae4dea54b2edbaa31536cba658b2484e64fdfb6
James Azam <jamesazam@sun.ac.za>
2023-02-03 21:40:20 +0000

    added the covid-19 SA data and associated doc

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' COVID-19 Data Repository for South Africa
+#'
+#' An aggregated subset of the COVID-19 Data Repository for South Africa created, 
+#' maintained and hosted by Data Science for Social Impact research group, 
+#' led by Dr. Vukosi Marivate ...
+#' 
+#' The data is originally provided as a linelist but has been subsetted and 
+#' cleaned in `data-raw/covid19_sa.R`.
+#'
+#' @format ## `covid19_sa`
+#' A data frame with 19 rows and 2 columns:
+#' \describe{
+#'   \item{date}{Date case was reported}
+#'   \item{cases}{Number of cases}
+#'   ...
+#' }
+#' @source <https://github.com/dsfsi/covid19za>
+#' Further details in `data-raw/covid19_sa.R`.
+"covid19_sa"
---
+++ data-raw/covid19_sa.R
@@ -0,0 +1,17 @@
+## code to prepare `covid_sa` dataset
+
+data_url <- 'https://raw.githubusercontent.com/dsfsi/covid19za/master/data/covid19za_timeline_confirmed.csv'
+
+#Read the data in using the url
+covid19_sa <- read.csv(data_url)
+
+#Clean and subset the data we need
+covid19_sa <- covid19_sa %>% 
+  dplyr::select(date) %>% 
+  dplyr::mutate(date = lubridate::dmy(date)) %>%
+  dplyr::filter(date <= lubridate::dmy('20-03-2020')) %>%   
+  dplyr::group_by(date) %>% 
+  dplyr::summarise(cases = n()) %>%   
+  dplyr::ungroup()
+
+usethis::use_data(covid19_sa, overwrite = TRUE)
