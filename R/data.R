28440c1e37b5535c315e24cce93998eb41b85ed5
James Azam <jamesazam@sun.ac.za>
2023-02-07 11:53:50 +0000

    removed a trailing ellipsis

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #'
 #' An aggregated subset of the COVID-19 Data Repository for South Africa created, 
 #' maintained and hosted by Data Science for Social Impact research group, 
-#' led by Dr. Vukosi Marivate ...
+#' led by Dr. Vukosi Marivate.
 #' 
 #' The data is originally provided as a linelist but has been subsetted and 
 #' cleaned in `data-raw/covid19_sa.R`.
