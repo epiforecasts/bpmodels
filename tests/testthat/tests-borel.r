960902c7da977c9503fe4931fd37117ce59a1aa1
James Azam <james.azam@lshtm.ac.uk>
2023-03-01 19:52:41 +0000

    lintr: changed expect_equal to expect_identical

--- tests/testthat/tests-borel.r
+++ tests/testthat/tests-borel.r
@@ -2,7 +2,7 @@ context("The Borel distribution is implemented")
 
 test_that("We can calculate probabilities and sample", {
   expect_gt(dborel(1, 0.5), 0)
-  expect_equal(dborel(1, 0.5, log = TRUE), -0.5)
+  expect_identical(dborel(1, 0.5, log = TRUE), -0.5)
   expect_length(rborel(2, 0.9), 2)
 })
 
