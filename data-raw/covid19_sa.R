0d7150509ac584362b0ed2718d10d0137455574d
James Azam <james.azam@lshtm.ac.uk>
2023-03-06 16:18:42 +0000

    linked covidza data to commit

--- data-raw/covid19_sa.R
+++ data-raw/covid19_sa.R
@@ -5,7 +5,7 @@ library(lubridate)
 library(usethis)
 
 # Link to data
-data_url <- "https://raw.githubusercontent.com/dsfsi/covid19za/master/data/covid19za_timeline_confirmed.csv" # nolint: line_length_linter.
+data_url <- "https://raw.githubusercontent.com/dsfsi/covid19za/1943f5e0d80fa296d9171ced473eebd3f2cde109/data/covid19za_timeline_confirmed.csv" # nolint: line_length_linter.
 
 # Read the data in using the url
 covid19_sa <- read.csv(data_url)
